7cfbaa059402be7b127ae17e1e26fca902e2cf84
Marc Abillama <mabillama@gmail.com>
2022-02-07 08:26:47 +0100

    Google OAuth added

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,51 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import googleIcon from "../assets/svg/googleIcon.svg";
+
+function OAuth() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      /* doc will take: 
+          1. the db we brought in from the config
+          2. the name of the collection, which is 'users'
+          3. and the user.uid
+          We're getting the user frmo the google sign in and passing the user.id to see if there's a reference */
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      // If user doesn't exist, create user
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  };
+
+  return (
+    <div className="socialLogin">
+      <p>Sign {location.pathname === "/sign-up" ? "up" : "in"} with </p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img className="socialIconImg" src={googleIcon} alt="google" />
+      </button>
+    </div>
+  );
+}
+
+export default OAuth;
