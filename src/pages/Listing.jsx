5c28b350d5f1e89bef682b86ebc8f217d310d013
Marc Abillama <mabillama@gmail.com>
2022-02-08 09:43:29 +0100

    Leaflet listing location on map added

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
@@ -63,7 +64,7 @@ function Listing() {
                 .toString()
                 .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </p>
-        <p className="lsitingLocation">{listing.location}</p>
+        <p className="listingLocation">{listing.location}</p>
         <p className="listingType">
           For {listing.type === "rent" ? "Rent" : "Sale"}
         </p>
@@ -83,7 +84,26 @@ function Listing() {
           <li>{listing.furnished && "Furnished"}</li>
 
           <p className="listingLocationTitle">Location</p>
-          {/* MAP */}
+
+          <div className="leafletContainer">
+            <MapContainer
+              style={{ height: "100%", width: "100%" }}
+              center={[listing.geolocation.lat, listing.geolocation.lng]}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+              />
+
+              <Marker
+                position={[listing.geolocation.lat, listing.geolocation.lng]}
+              >
+                <Popup>{listing.location}</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
 
           {auth.currentUser?.uid !== listing.userRef && (
             <Link
